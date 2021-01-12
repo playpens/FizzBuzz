053ec07e78a0dc277c0996e52876057c90e06a3c
John Cokos <highlander.44@gmail.com>
2021-01-12 11:49:05 -0800

    We did the awesome work for FB'ing numbers

--- Program.cs
+++ Program.cs
@@ -2,11 +2,45 @@
 
 namespace FizzBuzz
 {
-  class Program
+  public class Program
   {
     static void Main(string[] args)
     {
       Console.WriteLine("Hello World!");
+    }
+
+    /// <summary>
+    /// Return a string (Fizz/Buzz/FizzBuzz/Value) depending on the number sent
+    /// </summary>
+    /// <param name="v">Number to convert</param>
+    /// <returns>
+    /// If number % 3 -- Fizz
+    /// If number % 5 -- Buzz
+    /// If number % 3 & 5 -- FizzBuzz
+    /// Otherwise the number
+    /// </returns>
+    public static string Convert(int v)
+    {
+
+      if (v % 15 == 0)
+      {
+        return "FizzBuzz";
+
+      }
+
+      if (v % 3 == 0)
+      {
+        return "Fizz";
+      }
+
+      if (v % 5 == 0)
+      {
+        return "Buzz";
+      }
+
+
+
+      return v.ToString();
     }
   }
 }
